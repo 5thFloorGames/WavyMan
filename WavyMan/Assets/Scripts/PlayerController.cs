c035d0a6295413282dafb48b5fc040acc79c6fa0
41407 <jimi.welling@cs.helsinki.fi>
2017-01-20 21:29:12 +0200

    Add player controller, basic movement

---
+++ WavyMan/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody bod;
+    public Vector3 jumpForce;
+    private bool grounded = true;
+
+    void Awake()
+    {
+        bod = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        HandleInput();
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        grounded = true;
+    }
+
+    private void HandleInput()
+    {
+        transform.Translate(new Vector2(Input.GetAxisRaw("Horizontal") * Time.deltaTime, 0));
+        if (Input.GetButtonUp("Jump") && grounded)
+        {
+            grounded = false;
+            bod.velocity = jumpForce;
+        }
+    }
+}
