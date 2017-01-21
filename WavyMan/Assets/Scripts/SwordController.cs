b6e629583dea3fac0501aef9010daefc811da19d
Vamiyn <vamiynr@gmail.com>
2017-01-22 00:01:36 +0200

    bursts

---
+++ WavyMan/Assets/Scripts/SwordController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordController : MonoBehaviour {
+
+	public float burstSpeed;
+    public GameObject swordTrailNode;
+    private GameObject currentNode = null;
+    private GameObject previousNode = null;
+    public ParticleSystem particles;
+
+	private List<GameObject> nodeList = new List<GameObject>();
+    
+	void Start () {
+		particles.Stop();
+	}
+
+	void FixedUpdate () {
+        if(Input.GetButton("Swing")) {
+            if(!particles.isEmitting) {
+				BurstOff ();
+                particles.Play();
+            }
+            previousNode = currentNode;
+            currentNode = Instantiate(swordTrailNode, transform.position, Quaternion.identity);
+            currentNode.GetComponent<TrailNodeController>().SetPreviousNode(previousNode);
+			currentNode.GetComponent<TrailNodeController> ().SetMaster (gameObject);
+			nodeList.Add (currentNode);
+
+        } else if(particles.isEmitting){
+			particles.Stop();
+			BurstOn ();
+        }
+	}
+
+	public void ChildDying(GameObject obj) {
+		nodeList.Remove (obj);
+	}
+
+	void BurstOn() {
+		if (burstSpeed != 1.0f) {
+			var main = particles.main;
+			main.simulationSpeed = burstSpeed;
+			setSpeedMultipliers (burstSpeed);
+		}
+	}
+
+	void BurstOff() {
+		if (burstSpeed != 1.0f) {
+			var main = particles.main;
+			main.simulationSpeed = 1.0f;
+			setSpeedMultipliers (1.0f);
+		}
+	}
+
+	void setSpeedMultipliers(float multiplier) {
+		foreach (GameObject obj in nodeList) {
+			obj.SendMessage ("SetSpeedMultiplier", multiplier);
+		}
+	}
+}
---
+++ WavyMan/Assets/Scripts/TrailNodeController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailNodeController : MonoBehaviour
+{
+    public Vector3 translation;
+    public float timeToLive = 10;
+    public float strength;
+	public GameObject master;
+
+	private float speedMultiplier;
+
+    void OnEnable()
+    {
+		speedMultiplier = 1.0f;
+        StartCoroutine(DisableAfterDelay(timeToLive));
+    }
+
+    void Update()
+    {
+		transform.position += (translation * Time.deltaTime * speedMultiplier);
+    }
+
+    public void SetPreviousNode(GameObject node)
+    {
+        if (node != null)
+        {
+            transform.LookAt(node.transform);
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, Vector3.Distance(transform.position, node.transform.position) * 1.1f);
+        }
+    }
+
+    public void SetStrength(float strength)
+    {
+        this.strength = strength;
+        //transform.GetChild(1).localScale = Vector3.one * strength;
+    }
+
+	public void SetMaster(GameObject obj) {
+		master = obj;
+	}
+
+	public void SetSpeedMultiplier(float multiplier) {
+		speedMultiplier = multiplier;
+	}
+
+    private IEnumerator DisableAfterDelay(float time)
+    {
+        yield return new WaitForSeconds(time);
+		if (gameObject.tag == "Wave") {
+			master.GetComponent<SwordController> ().ChildDying(gameObject);
+		}
+        gameObject.SetActive(false);
+    }
+}
