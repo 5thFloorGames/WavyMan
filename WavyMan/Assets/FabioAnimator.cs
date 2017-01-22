24d12e1b50d7aa080357d4f79aa84e7461c2cf1c
Vamiyn <vamiynr@gmail.com>
2017-01-22 15:19:53 +0200

    arms are controllable

---
+++ WavyMan/Assets/FabioAnimator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FabioAnimator : MonoBehaviour {
+
+	Animator armAnimator;
+	float num;
+
+	void Start () {
+		armAnimator = transform.FindChild ("Arms").GetComponent<Animator> ();
+		num = 0;
+		ArmPosition (0);
+	}
+
+	void Update () {
+	}
+
+	public void ArmPosition(float positionNum) {
+		armAnimator.Play("wavymanArms", 0, positionNum/8.0f);
+
+	}
+}
