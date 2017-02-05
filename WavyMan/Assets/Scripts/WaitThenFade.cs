fe766df40623f35de37c2d5ff62defa2f0c46e89
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2017-02-05 17:54:34 +0200

    starting to add credits

---
+++ WavyMan/Assets/Scripts/WaitThenFade.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaitThenFade : MonoBehaviour {
+
+	private Image image;
+
+	// Use this for initialization
+	void Start () {
+		image = GetComponent<Image>();
+		StartCoroutine(WaitFade());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator WaitFade (){
+		yield return new WaitForSeconds(5);
+		Color oldColor = image.color;
+		while(image.color.a > 0){
+			image.color = new Color(oldColor.r, oldColor.b, oldColor.g, image.color.a - 0.01f);
+			yield return null;
+		}
+	}
+}
